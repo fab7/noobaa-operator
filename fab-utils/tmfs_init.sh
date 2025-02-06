ae84ae250073b5af3d9a90e8049edcfa2c15f5e7
Francois Abel <fab@zurich.ibm.com>
2025-02-06 22:30:38 +0100

    Improved SCSI dev mngt

--- fab-utils/tmfs_init.sh
+++ fab-utils/tmfs_init.sh
@@ -16,7 +16,7 @@ export TMFS_DATA_DIR="/noobaa_storage"
 if [ ! -d "${TMFS_DATA_DIR}" ]; then
   echo "[WARNING] Directory '${TMFS_DATA_DIR}' does not exist..."
   exit 1
-elif [ ${VERBOSE} -eq 1 ]; then
+elif [ ${VERBOSE} -gt 0 ]; then
     echo "[INFO] TMFS data directory is: '${TMFS_DATA_DIR}'"
     echo
 fi
@@ -27,7 +27,7 @@ mkdir -p ${TMFS_WORK_DIR}
 if [ $? -ne 0 ]; then
     echo "[ERROR] Failed to create directory '${TMFS_WORK_DIR}' "
     exit 1
-elif [ ${VERBOSE} -eq 1 ]; then
+elif [ ${VERBOSE} -gt 0 ]; then
     echo "[INFO] TMFS_WORK_DIR directory is: '${TMFS_WORK_DIR}' "
     echo
 fi
@@ -38,7 +38,7 @@ mkdir -p ${TMFS_LOG_DIR}
 if [ $? -ne 0 ]; then
     echo "[ERROR] Failed to create directory '${TMFS_LOG_DIR}' "
     exit 1
-elif [ ${VERBOSE} -eq 1 ]; then
+elif [ ${VERBOSE} -gt 0 ]; then
     echo "[INFO] TMFS_LOG_DIR directory is: '${TMFS_LOG_DIR}' "
     echo
 fi
@@ -49,7 +49,7 @@ export BASEDEV=$(echo ${BASEDEV} | sed 's/,/\\,/g')
 if [ -z "${BASEDEV}" ]; then
     echo "[ERROR] BASEDEV is is empty."
     exit 1;
-elif [ ${VERBOSE} -eq 1 ]; then
+elif [ ${VERBOSE} -gt 0 ]; then
     echo "[INFO] BASEDEV=${BASEDEV}"
     echo 
 fi
@@ -58,142 +58,185 @@ echo "#-- Setting ADMIN_KEY ------------------------------------"
 if [ -z "${CEPH_ADMIN_KEY}" ]; then
     echo "[WARNING] Secret CEPH_ADMIN_KEY is not set..."
     exit 1
-elif [ ${VERBOSE} -eq 1 ]; then
+elif [ ${VERBOSE} -gt 0 ]; then
     echo "[INFO] CEPH_ADMIN_KEY=${CEPH_ADMIN_KEY}"
     echo
 fi
 
 echo "#-- Setting TMFS mount options ---------------------------"
 MOUNT_OPTIONS="name=admin\\,secret=${CEPH_ADMIN_KEY}"
-if [ ${VERBOSE} -eq 1 ]; then
+if [ ${VERBOSE} -gt 0 ]; then
     echo "[INFO] The TMFS mount options are: '${MOUNT_OPTIONS}' "
     echo
 fi 
 
-# [OBSOLETE - This part is now handled by the initContainer]
-# echo "#-- Re-Scanning the SCSI Bus -----------------------------"
-# #-- This operation might be required if you changed any HW config
-# SCSI_HOSTS=($(ls /sys/class/scsi_host))
-# if [[ ! ${SCSI_HOSTS[0]} =~ "host" ]]; then
-#     echo "[WARNING-ERROR] No SCSI host adapter found!"
-#     exit 1
-# elif [ ${VERBOSE} -eq 1 ]; then
-#     echo "[INFO] Found ${#SCSI_HOSTS[@]} SCSI host adapter ports"
-# fi
-# #-- WARNING: This operation should be performed on the Host (Safer)
-# sudo mount -o remount,rw /sys
-# for host in "${SCSI_HOSTS[@]}"; do
-#     echo "- - -" | sudo tee /sys/class/scsi_host/${host}/scan 2>&1 > /dev/null
-#     if [ $? -ne 0 ]; then
-#         echo "[ERROR] Failed to re-scan SCSI host adapter port: ${host}"
-#         exit 1
-#     elif [ ${VERBOSE} -eq 1 ]; then
-#         echo "[INFO] Re-scanned SCSI host adapter port: ${host}"
-#     fi
-# done
-# sudo mount -o remount,ro /sys 
-# # echo
+# [INFO]
+# The next 2 sections will identify the medium changer and the tape drive
+# devices to be used by the BackingStore container. The 3rd section will then
+# check if a SCSI reservation is in place and will clear it if required.
+#
+# [WARNING-TODO-FIXME]
+# At this point, we should interact with the TS4500 tape library to find out
+# which tape drive is assigned to which logical library. However, for the time
+# being, we will assume the following configuration:
+#  - there is *ONLY ONE* tape drive assigned per logical library,
+#  - the assigned tape drive is *ALWAYS* point-2-point attached to the first
+#    port (i.e. port #0) of the *FIRST* host buffer adapter.
+
+echo "#-- Retrieve HOST BUFFER ADAPTERS ------------------------"
+PCI_ADDRS=$(lspci | grep -i "Fibre Channel" | awk '{print $1}')
+readarray -t PCI_ADDRS <<< "${PCI_ADDRS}"
+if [[ ${#PCI_ADDRS[@]} = 0 ]]; then
+    echo "[WARNING-ERROR] No Fibre Channel host buffer adapter found!"
+    exit 1
+elif [ ${VERBOSE} -gt 0 ]; then
+    echo "[INFO] Found ${#PCI_ADDRS[@]} Fibre Channel ports"
+fi
+echo "[INFO] Selecting Fibre Channel HBA located at PCI address '${PCI_ADDRS[0]}'"
+
+echo "#-- Retrieve SCSI_HOST -----------------------------------"
+SCSI_HOST_NAME=$(basename /sys/bus/pci/devices/0000:${PCI_ADDRS[0]}/host*)
+if [ $? -ne 0 ]; then
+    echo "[ERROR] Failed to retrieve the SCSI host name for PCI address '${PCI_ADDR[0]}'"
+    exit 1
+fi
+#-- Extract the SCSI host number
+SCSI_HOST="${SCSI_HOST_NAME:4}"
+echo "[INFO] The corresponding SCSI host is 'host${SCSI_HOST}'"
 
 echo "#-- Retrieve MEDIUM CHANGERS -----------------------------"
 DEVICE="mediumx"; VENDOR="IBM"; PRODUCT="03584L32"
-PHY_CHANGERS=$(lsscsi -g | grep ${DEVICE} | grep ${VENDOR} | grep ${PRODUCT} | awk '{print $(NF-1)}')
-GEN_CHANGERS=$(lsscsi -g | grep ${DEVICE} | grep ${VENDOR} | grep ${PRODUCT} | awk '{print $(NF)}')
+PHY_CHANGERS=$(lsscsi -g | grep "^\[${SCSI_HOST}:" | grep ${DEVICE} | awk '{print $(NF-1)}')
+GEN_CHANGERS=$(lsscsi -g | grep "^\[${SCSI_HOST}:" | grep ${DEVICE} | awk '{print $(NF)}')
 readarray -t PHY_CHANGERS <<< "${PHY_CHANGERS}"
 readarray -t GEN_CHANGERS <<< "${GEN_CHANGERS}"
-#-- Retrieve the 1st physical IBM Medium Changer"
+#-- Retrieve the physical IBM Medium Changers
 if [[ ${#PHY_CHANGERS[@]} = 0 ]]; then
-    echo "[WARNING-ERROR] No physical medium changer device found!"
+    echo "[ERROR] No physical medium changer device found!"
     exit 1
-elif [[ ${PHY_CHANGERS[0]} != "/dev/sch0" ]]; then
-    echo "[WARNING-ERROR] Expecting physical medium changer device to be '/dev/sch0' but found '${PHY_CHANGERS[0]}'!"
-    exit 1
-elif [ ${VERBOSE} -eq 1 ]; then
-    echo "[INFO] Found ${#PHY_CHANGERS[@]} medium changers"
+else
+    for PHY_CHANGER in "${PHY_CHANGERS}"; do
+        if [[ ! ${PHY_CHANGER} =~ "/dev/sch" ]]; then
+            echo "[ERROR] Expecting physical medium changer device name to match '/dev/sch' but found '${PHY_CHANGER}'!"
+            exit 1
+        fi
+    done
 fi
-#-- Retrieve the 1st generic IBM Medium Changer"
+#-- Retrieve the generic IBM Medium Changers
 if [[ ${#GEN_CHANGERS[@]} = 0 ]]; then
-    echo "[WARNING-ERROR] No generic medium changer device found!"
+    echo "[ERROR] No generic medium changer device found!"
     exit 1
-elif [[ ! ${GEN_CHANGERS[0]} =~ "/dev/sg" ]]; then
-    echo "[WARNING-ERROR] Expecting generic medium changer device name to match '/dev/sg' but found '${GEN_CHANGERS[0]}'!"
-    exit 1
-elif [ ${VERBOSE} -eq 1 ]; then
-    echo "[INFO] Assuming medium device changer to be '${GEN_CHANGERS[0]}' "
+else
+    for GEN_CHANGER in "${GEN_CHANGERS}"; do
+        if [[ ! ${GEN_CHANGER} =~ "/dev/sg" ]]; then
+            echo "[ERROR] Expecting generic medium changer device name to match '/dev/sg' but found '${GEN_CHANGER}'!"
+            exit 1
+        fi
+    done
+fi
+#-- Only keep the 1st generic IBM Medium Changer
+GEN_CHANGER=${GEN_CHANGERS[0]}
+if [ ${VERBOSE} -gt 0 ]; then
+    if [[ ${#GEN_CHANGERS[@]} -gt 1 ]]; then
+        echo "[INFO] Found ${#GEN_CHANGERS[@]} medium changers."
+        echo "[INFO] Selecting the first one: '${GEN_CHANGER}'"
+    else
+        echo "[INFO] Found medium changer to be '${GEN_CHANGER}'"
+    fi
 fi
 
 echo "#-- Retrieve TAPE DRIVES ---------------------------------"
 DEVICE="tape"; VENDOR="IBM"; PRODUCT="ULT3580-TD9"
-PHY_DRIVES=$(lsscsi -g | grep ${DEVICE} | grep ${VENDOR} | grep ${PRODUCT} | awk '{print $(NF-1)}')
-GEN_DRIVES=$(lsscsi -g | grep ${DEVICE} | grep ${VENDOR} | grep ${PRODUCT} | awk '{print $(NF)}')
+PHY_DRIVES=$(lsscsi -g | grep "^\[${SCSI_HOST}:" | grep ${DEVICE} | awk '{print $(NF-1)}')
+GEN_DRIVES=$(lsscsi -g | grep "^\[${SCSI_HOST}:" | grep ${DEVICE} | awk '{print $(NF)}')
 readarray -t PHY_DRIVES <<< "${PHY_DRIVES}"
 readarray -t GEN_DRIVES <<< "${GEN_DRIVES}"
-#-- Retrieve the 1st physical IBM Tape Drive"
+#-- Retrieve the physical IBM Tape Drives
 if [[ ${#PHY_DRIVES[@]} = 0 ]]; then
-    echo "[WARNING-ERROR] No physical tape drive device found!"
+    echo "[ERROR] No physical tape drive device found!"
     exit 1
-elif [[ ! ${PHY_DRIVES[0]} =~ "/dev/st" ]]; then
-    echo "[WARNING-ERROR] Expecting physical tape drive name to match '/dev/st' but found '${PHY_DRIVES[0]}'!"
-    exit 1
-elif [ ${VERBOSE} -eq 1 ]; then
-    echo "[INFO] Found ${#PHY_DRIVES[@]} tape drives"
+else
+    for PHY_DRIVE in "${PHY_DRIVES}"; do
+        if [[ ! ${PHY_DRIVE} =~ "/dev/st" ]]; then
+            echo "[ERROR] Expecting physical tape drive name to match '/dev/st' but found '${PHY_DRIVE}'!"
+            exit 1
+        fi
+    done     
 fi
-#-- Retrieve the 1st generic IBM Tape Drive"
+#-- Retrieve the generic IBM Tape Drives
 if [[ ${#GEN_DRIVES[@]} = 0 ]]; then
-    echo "[WARNING-ERROR] No generic tape drive device found!"
+    echo "[ERROR] No generic tape drive device found!"
     exit 1
-elif [[ ! ${GEN_DRIVES[0]} =~ "/dev/sg" ]]; then
-    echo "[WARNING-ERROR] Expecting generic tape drive device name to match '/dev/sg' but found '${GEN_DRIVES[0]}'!"
-    exit 1
-elif [ ${VERBOSE} -eq 1 ]; then
-    echo "[INFO] Assuming tape drive to be '${GEN_DRIVES[0]}' "
+else
+    for GEN_DRIVE in "${GEN_DRIVES}"; do
+        if [[ ! ${GEN_DRIVE} =~ "/dev/sg" ]]; then
+            echo "[ERROR] Expecting generic tape drive device name to match '/dev/sg' but found '${GEN_DRIVE}'!"
+            exit 1
+        fi
+    done
+fi
+#-- Only keep the 1st generic IBM Tape Drive
+GEN_DRIVE=${GEN_DRIVE[0]}
+if [ ${VERBOSE} -gt 0 ]; then
+    if [[ ${#GEN_DRIVES[@]} -gt 1 ]]; then
+        echo "[INFO] Found ${#GEN_DRIVES[@]} tape drives."
+        echo "[INFO] Selecting the first one: '${GEN_DRIVE}'"
+    else
+        echo "[INFO] Found tape drive to be '${GEN_DRIVE}'"
+    fi
 fi
 
-echo "#-- Clear existing SCSI reservations in TAPE DRIVES ------"
-for TAPE_DRIVE in "${GEN_DRIVES[0]}"; do
-    # Read Existing Reservation
-    RES=$(sudo sg_persist --in --read-reservation ${TAPE_DRIVE})
+echo "#-- Check and clear existing SCSI reservations -----------"
+GEN_DEVS=("${GEN_CHANGER}" "${GEN_DRIVE}")
+for GEN_DEV in "${GEN_DEVS[@]}"; do
+    RES=$(sudo sg_persist --in --read-reservation ${GEN_DEV})
+    if [[ ${RES} =~ "Unit attention" ]]; then
+        echo "[WARNING] The SCSI device '${GEN_DEV}'experienced a Unit Attention condition in the past. ${RES}"
+        echo "[INFO] Trying to read the SCSI reservation a second time..."
+        RES=$(sudo sg_persist --in --read-reservation ${GEN_DEV})
+    fi
     if [ $? -ne 0 ]; then
-        echo "[ERROR] Failed to read SCSI reservations for '${TAPE_DRIVE}'"
+        echo "[ERROR] Failed to read SCSI reservations for '${GEN_DEV}'"
         exit 1
-    elif [ ${VERBOSE} -eq 1 ]; then
-        echo "[INFO] Current SCSI reservation for '${TAPE_DRIVE}' "
+    elif [ ${VERBOSE} -gt 0 ]; then
+        echo "[INFO] Current SCSI reservation for '${GEN_DEV}' "
         echo "${RES}"
     fi
     if [[ ${RES} =~ "Reservation follows:" ]]; then
-        echo "[WARNING] A SCSI reservations is in place for '${TAPE_DRIVE}' "
+        echo "[WARNING] A SCSI reservations is in place for '${GEN_DEV}' "
         # Retrieve the reservation type
         RES_TYPE=$(echo "${RES}" | grep -oP ',  type: \K.*')
         if [ $? -ne 0 ]; then
-            echo "[ERROR] Failed to retrieve the SCSI reservation type for '${TAPE_DRIVE}' "
+            echo "[ERROR] Failed to retrieve the SCSI reservation type for '${GEN_DEV}' "
             exit 1
-        elif [ ${VERBOSE} -eq 1 ]; then
-            echo "[INFO] The reservation type for '${TAPE_DRIVE}' is: '${RES_TYPE}'"
+        elif [ ${VERBOSE} -gt 0 ]; then
+            echo "[INFO] The reservation type for '${GEN_DEV}' is: '${RES_TYPE}'"
         fi
         # Retrieve the reservation key
         RES_KEY=$(echo "${RES}" | grep -oP 'Key=\K0x[0-9a-fA-F]+')
         if [ $? -ne 0 ]; then
-            echo "[ERROR] Failed to retrieve the reservation key for '${TAPE_DRIVE}' "
+            echo "[ERROR] Failed to retrieve the reservation key for '${GEN_DEV}' "
             exit 1
-        elif [ ${VERBOSE} -eq 1 ]; then
-            echo "[INFO] The reservation key for '${TAPE_DRIVE}' is: '${RES_KEY}'"
+        elif [ ${VERBOSE} -gt 0 ]; then
+            echo "[INFO] The reservation key for  '${GEN_DEV}' is: '${RES_KEY}'"
         fi
         # Clear the reservation
         if [[ "${RES_TYPE}" == "Exclusive Access" ]]; then
-            RES=$(sudo sg_persist --out --clear --param-rk=${RES_KEY} ${TAPE_DRIVE})
+            RES=$(sudo sg_persist --out --clear --param-rk=${RES_KEY} ${GEN_DEV})
             if [ $? -ne 0 ]; then
-                echo "[ERROR] Failed to clear the SCSI '${RES_TYPE}}' reservation for '${TAPE_DRIVE}'"
+                echo "[ERROR] Failed to clear the SCSI '${RES_TYPE}}' reservation for '${GEN_DEV}'"
                 exit 1
-            elif [ ${VERBOSE} -eq 1 ]; then
-                echo "[INFO] Cleared the SCSI '${RES_TYPE}}' reservation for '${TAPE_DRIVE}'"
+            elif [ ${VERBOSE} -gt 0 ]; then
+                echo "[INFO] Cleared the SCSI '${RES_TYPE}}' reservation for '${GEN_DEV}'"
             fi
         else
             echo "[ERROR] The clearing of the SCSI reservation type '${RES_TYPE}' is not supported yet..."
             exit 1
         fi
         # Check if the reservation has been cleared
-        RES=$(sudo sg_persist --in --read-reservation ${TAPE_DRIVE})
+        RES=$(sudo sg_persist --in --read-reservation ${GEN_DEV})
         if [ $? -ne 0 ]; then
-            echo "[ERROR] Failed to read SCSI reservations for '${TAPE_DRIVE}'"
+            echo "[ERROR] Failed to read SCSI reservations for '${GEN_DEV}'"
             exit 1
         elif [[ ! ${RES} =~ "there is NO reservation held" ]]; then
             echo "[ERROR] One or more SCSI reservation(s) still pending... "
@@ -214,8 +257,8 @@ else
     echo -e "\t TMFS log  directory is      : '${TMFS_LOG_DIR}' "
     echo -e "\t BASEDEV is                  : '${BASEDEV}' "
     echo -e "\t The TMFS mount options are  : '${MOUNT_OPTIONS}' "
-    echo -e "\t The medium changer device is: '${CHANGER}' "
-    echo -e "\t The tape drive device is    : '${TAPE_DRIVES[0]}' "
+    echo -e "\t The medium changer device is: '${GEN_CHANGER}' "
+    echo -e "\t The tape drive device is    : '${GEN_DRIVE}' "
     echo
     # Note: Allow non-root user to access Fuse filesystem is done during 'noobaa-core' build
     if true; then
@@ -229,16 +272,17 @@ else
         else
             echo "[INFO] '${TMFS_DATA_DIR}' was not mounted"
         fi
-        # Starting TMFS
-        if [ ${VERBOSE} -eq 1 ]; then
+        if [ ${VERBOSE} -eq 2 ]; then
+            # Starting TMFS in foreground mode
             sudo TMFS_WORK_DIR=${TMFS_WORK_DIR} /usr/local/bin/tmfs -f \
                 -o allow_other -o basedev="${BASEDEV}" \
-                -o mountoptions="${MOUNT_OPTIONS}" -o changer_devname="${CHANGER}" \
+                -o mountoptions="${MOUNT_OPTIONS}" -o changer_devname="${GEN_CHANGER}" \
                 -o mig_wait_sec=0 ${TMFS_DATA_DIR}
         else
+            # Starting TMFS in background mode
             sudo TMFS_WORK_DIR=${TMFS_WORK_DIR} /usr/local/bin/tmfs \
                 -o allow_other -o basedev="${BASEDEV}" \
-                -o mountoptions="${MOUNT_OPTIONS}" -o changer_devname="${CHANGER}" \
+                -o mountoptions="${MOUNT_OPTIONS}" -o changer_devname="${GEN_CHANGER}" \
                 -o mig_wait_sec=0 ${TMFS_DATA_DIR}  > "${TMFS_LOG_DIR}/tmfs.log" 2>&1
         fi
         echo "[INFO] 'TMFS successfully started"

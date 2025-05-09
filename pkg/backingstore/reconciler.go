aa58ae67dd5ec115a8546f2fe0b025d97c7ee0c4
Francois Abel <fab@zurich.ibm.com>
2025-05-09 21:39:15 +0200

    Add rook-cephfs-1d3m

--- pkg/backingstore/reconciler.go
+++ pkg/backingstore/reconciler.go
@@ -1302,9 +1302,9 @@ func (r *Reconciler) reconcileMissingTmfsPvcs(pvcsList, tmfsPvcsList *corev1.Per
 		storageClassName := "rook-cephfs"
 		newPvc.Spec.StorageClassName = &storageClassName
 		r.Own(newPvc)
-		// println("[UTK]: reconcileMissingTmfs - Trying to create PVC", i)
+		println("[FAB]: reconcileMissingTmfs - Trying to create PVC: ", newPvc.Name)
 		util.KubeCreateSkipExisting(newPvc)
-		// println("[UTK]: reconcileMissingTmfs - Created PVC", i)
+		println("[FAB]: reconcileMissingTmfs - Created PVC: ", newPvc.Name)
 	}
 	return nil
 }
